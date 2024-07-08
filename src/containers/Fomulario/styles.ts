2e5b9fe8c554c8f6adf778018bce1023d58f850a
jonesnambunndo@hotmail.com <jonesnambundo@mtel.inatel.br>
2024-07-08 14:44:20 -0300

    projeto finalizadio

---
+++ src/containers/Fomulario/styles.ts
@@ -0,0 +1,25 @@
+import styled from 'styled-components'
+
+export const Form = styled.form`
+  max-width: 547 px;
+  width: 100%;
+  font-weight: bold;
+  font-size: 14px;
+
+  textarea {
+    resize: none;
+    margin: 16px 0;
+  }
+`
+export const Opcoes = styled.div`
+  margin-bottom: 16px;
+
+  p {
+    margin: 0 10px 0 5px;
+  }
+`
+
+export const Opcao = styled.div`
+  display: inline;
+  text-transform: capitalize;
+`
